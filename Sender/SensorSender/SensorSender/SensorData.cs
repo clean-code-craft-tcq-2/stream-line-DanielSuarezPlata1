e0512cce29e43ed612b43385824ccda442206456
Suarez Plata Daniel (RBEI/EBS11-SX) <Daniel.SuarezPlata@mx.bosch.com>
2022-04-06 12:57:18 -0500

    Sender program

---
+++ Sender/SensorSender/SensorSender/SensorData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SensorSender
+{
+    public class SensorData
+    {
+        public float temperature { get; set; }
+        public float stateOfCharge { get; set; }
+    }
+}
